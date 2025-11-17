4b961054aa5b8198cf1f505d17243007d0e04a93
GabrielOrt2025 <gortega00053@ufide.ac.cr>
2025-11-17 01:33:00 -0600

    sobreNosotros

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def home():
     return render_template('home.html')
+
+
+@bp.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
