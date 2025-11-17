0514280b0e17c7f6c530f054433f3c2a4750e26b
Jose S.C <jsequeira00582@ufide.ac.cr>
2025-11-17 00:29:56 -0600

    Carrito (Procedimiento de venta) Perfil FRONT-END
    Ahi esta el carrito y el perfil xd

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def home():
     return render_template('home.html')
+
+@bp.route('/carrito')
+def carrito():
+    return render_template('carrito.html')
+
+@bp.route('/cuenta')
+def cuenta():
+    return render_template('cuenta.html')
